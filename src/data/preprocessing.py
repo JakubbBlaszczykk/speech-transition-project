cb1df0a25ce47e892db558e52a561508afe7dd9f
Jakub_Blaszczyk <250380@edu.p.lodz.pl>
2026-04-19 21:50:56 +0100

    add processing update, final model with documentation of the experiments

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -17,15 +17,24 @@ def process_file(file_path, output_path):
         start = random.randint(0, len(audio) - 3 * sr)
         audio = audio[start:start + 3 * sr]
 
-        # mel spectrogram
-        spec = librosa.feature.melspectrogram(y=audio, sr=sr)
-        spec_db = librosa.power_to_db(spec, ref=np.max)
+        # MFCC
+        mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=40)
 
-        # normalzation 
-        spec_db = (spec_db - np.mean(spec_db)) / np.std(spec_db)
+        # DELTA (time changes)
+        delta = librosa.feature.delta(mfcc)
+
+        # STACK (80 x time)
+        features = np.vstack([mfcc, delta])
+
+        # NORMALIZATION
+        std = np.std(features)
+        if std == 0:
+            std = 1e-6
+
+        features = (features - np.mean(features)) / std
 
         # save
-        np.save(output_path, spec_db)
+        np.save(output_path, features)
 
     except Exception as e:
         print("Error:", file_path, e)
@@ -36,11 +45,14 @@ def process_folder(input_dir, output_dir):
 
     files = [f for f in os.listdir(input_dir) if f.endswith(".mp3")]
 
+    print(f"Processing {input_dir}, found {len(files)} files")
+
     for file in tqdm(files):
         input_path = os.path.join(input_dir, file)
         output_path = os.path.join(output_dir, file.replace(".mp3", ".npy"))
 
         process_file(input_path, output_path)
+        
 
 
 if __name__ == "__main__":
